0fb38aed1c84149dc0df4d10b9434e1075a6054e
Mat467 <mat12368957@gmail.com>
2026-05-07 00:23:13 +0200

    db.py

--- db.py
+++ db.py
@@ -136,5 +136,15 @@ def set_casino_count(user_id: int, value: int):
     supabase.table("users").update({
         "casino_count": value
     }).eq("user_id", str(user_id)).execute()
+    
+def get_coinflip_count(user_id: int):
+    ensure_user(user_id)
+    res = supabase.table("users").select("coinflip_count").eq("user_id", str(user_id)).execute()
+    return res.data[0]["coinflip_count"] or 0
 
+
+def set_coinflip_count(user_id: int, value: int):
+    supabase.table("users").update({
+        "coinflip_count": value
+    }).eq("user_id", str(user_id)).execute()
 
