805737a1dba1ec12ad8dffcf828de3315e0191bf
Mat467 <mat12368957@gmail.com>
2026-07-09 12:36:21 +0200

    bot_web_keepalive.py

--- bot_web_keepalive.py
+++ bot_web_keepalive.py
@@ -267,10 +267,14 @@ async def ladder_system_task():
                 await asyncio.sleep(60)
                 continue
 
-
             for user in users:
-                user_id = int(user["user_id"])
-                balance = user["balance"]
+                raw_user_id = str(user.get("user_id", ""))
+
+                if not raw_user_id.isdigit():
+                continue
+
+                user_id = int(raw_user_id)
+                balance = int(user.get("balance") or 0)
                 new_ladder, _ = get_ladder(balance)
                 old_ladder = user.get("ladder")
 
